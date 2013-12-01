00133370e0ee76436321189313780ceddd449262
stealth <stealthw@mail.ru>
2013-12-01 08:59:56 +0200

    lesson 1 2

--- lesson1.rb
+++ lesson1.rb
@@ -1,2 +1,61 @@
 puts "test"
 puts 2 + 2
+
+#puts "text".methods
+
+print "print00  "
+
+printf "fghgfjh11  \n"
+
+p "gfhfdjhj12  "
+
+"kcxhlkfjbgb123  ".display
+
+p [1,2,3,4]
+
+puts 5
+puts "5"
+
+p 7
+p "7"
+
+puts [1,2,3,4,5]
+
+# p 'привет'
+
+p '********************'
+
+
+# peremennie
+p "peremennie"
+
+massiv = [1,2,3,4,5]
+
+p massiv
+puts massiv
+p "*******************"
+
+peremennaya = massiv + [1,2]
+p peremennaya
+p massiv
+p "***"
+peremennaya2 = peremennaya - massiv     # ???????????????!!!!!
+p peremennaya2                          # ???????????????!!!!!
+p "***"
+
+# peremennie -> object
+podruga = "Dasha"
+k_nam_v_gosti_idet = podruga
+puts podruga
+
+k_nam_v_gosti_idet[0] = "M"
+puts podruga
+
+p"***"
+podruga = "Dasha"
+k_nam_v_gosti_idet = podruga.clone
+puts podruga
+
+k_nam_v_gosti_idet[0] = "M"
+puts podruga
+p"***"
---
+++ lesson2.rb
@@ -0,0 +1,36 @@
+puts 'lesson2'
+
+podruga = "Dasha"
+k_nam_v_gosti_idet = "Vanya"
+puts podruga
+p"***"
+
+# tipi dannih
+# Fixnumnum Bignum Float Array String Hash
+# Chisla
+p 5
+p -12
+p 4.5
+p 076 # 8 richnoe
+p 0b010 # 2  ichnoe
+p 0*89  # 16 ichnoe
+
+# logicheskiy
+p "|| ->  or"
+p "&& -> and"
+p "! -> not"
+p "^ -> xor"
+
+p "=="
+p "!="
+p "< > <= >="
+
+# massivi
+massiv = ["dkfugh", "654", ["jfg", "dfijgh"], "kjfg 654 dfh 654"]
+p massiv
+
+# stroki
+# .inspect .to_s
+# .gsub .match .scan .split
+
+puts "privet".gsub
