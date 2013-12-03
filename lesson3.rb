33526e332c759c3721b688c109b952e3f6760450
stealth <stealthw@mail.ru>
2013-12-03 07:24:12 +0200

    lesson 3 prodoljenie 2

--- lesson3.rb
+++ lesson3.rb
@@ -57,3 +57,67 @@ p "***"
 p " +  - "
 p [1,2,3,4] + [5,6] + [7,8]
 p [1,1,2,2,3,3,3,4,5] - [1,2,4]
+
+p "***** 2 chast *****"
+
+p " obedinenie | i peresechenie & massivov kak mnogestv"
+p [1,2,3,4,5,5,6] | [0,1,2,3,4,5,7]
+p [1,2,3,4,5,5,6] & [0,1,2,3,4,5,7]
+
+p "udalenie dublikatov .uniq"
+p [1,2,3,4,5,5,6,0,1,2,3,4,5,7].uniq
+
+p "spluschivanie .flatten"
+p massiv = [[1,2],[3,4]]
+p massiv.flatten
+p massiv.flatten.max
+p [[2,3,5],["dsf",768],[54,45,1,4,'ert']]
+p [[2,3,5],["dsf",768],[54,45,1,4,'ert']].flatten
+p [[2,3,5],["dsf",768],[54,45,1,4,'ert',["w","e",1,["sd","v","c","x",9]]]]
+p [[2,3,5],["dsf",768],[54,45,1,4,'ert',["w","e",1,["sd","v","c","x",9]]]].flatten
+
+p "transponirovanie"
+p "transponirovanie dla strok"
+p massiv_2d =[[1,2],[3,4]]
+p massiv_2d.map # !!! map
+p massiv_2d.map { |massiv| massiv.max} # !!! map
+
+p "transponirovanie dla 2d massiva"
+p massiv_2d =[[1,2],[3,4]]
+p massiv_2d.transpose.map{ |massiv| massiv.max }
+
+p "razmnojenie mas"
+p [111, 222, 333, 444]
+p [111, 222, 333, 444] * 2
+p massiv = [111, 222, 333, 444]
+p massiv + massiv
+
+p "stek"
+p massiv = [1,2,3,4,5]
+p massiv[massiv.size] = 6
+p massiv         # !!!
+p massiv[0...-1] # !!!
+p massiv         # !!!
+
+p ".push .pop"
+p massiv = [1,2,3,4,5]
+p massiv.push( 6 )
+p massiv         # !!!
+p massiv.pop     # !!!
+p massiv         # !!!
+
+p "ochered i spisok"
+p "[]  []=  +"
+p massiv = [1,2,3,4,5]
+p massiv = [6] + massiv
+p massiv[0]
+p massiv
+p massiv = massiv[1..-1]
+p "*"
+p massiv = [1,2,3,4,5]
+p massiv.unshift( 6 )
+p massiv
+p massiv.shift
+p massiv
+
+p ".shift .unshift .pop .push"
