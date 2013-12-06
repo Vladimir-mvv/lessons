26f1be040add5f96fbfee2dee99fce5598c9d8af
stealth <stealthw@mail.ru>
2013-12-06 08:31:37 +0200

    stroki lesson part 1

--- lesson5.rb
+++ lesson5.rb
@@ -1,1 +1,29 @@
-# stroki
+# stroki " "  ' ' #{}
+p "stroka"
+p'stroka'
+p "ya em #{'soma'}"
+# specsimvoli \  \n  \t  \\   \"
+p '\n'
+p "\n"
+p '***'
+
+p chislo = 9
+p massiv = [1,2,3,4]
+p 'chislo = ' + chislo.to_s + ', moy massiv dlinni ' + massiv.size.to_s
+p "chislo =  #{chislo}, moy massiv dlinni #{massiv.size} "
+p "chislo =  #{chislo}, moy massiv dlinni #{massiv.join(",")} "
+p "chislo =  #{chislo}, moy massiv dlinni #{massiv} "
+p "***"
+
+p "metodi raboti so strokami"
+p mass = [7,5,8,3,2,5,4,8,9]
+p mass.max
+p "Max element massiva = #{mass.max}"
+p '***'
+
+p chislo = 123
+p "v dvoichn vide %b"  % chislo # ???!!! zadeysvovali metod % analog sprintf Cu
+p '*'
+p "v dvoichn vide #{ chislo.to_s(2) }"
+p '***'
+
