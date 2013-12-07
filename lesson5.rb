933c7982025a82b88e67ba2ae33153af895bf6c6
stealth <stealthw@mail.ru>
2013-12-07 08:44:48 +0200

    esche stroki

--- lesson5.rb
+++ lesson5.rb
@@ -27,3 +27,50 @@ p '*'
 p "v dvoichn vide #{ chislo.to_s(2) }"
 p '***'
 
+p syprug = "Sasha"
+p sypruga = "Masha"
+p syprug + " plus " + sypruga + " = semya"
+p "***"
+p let = 180
+p "ky-ky! " * let
+p "***"
+
+class String
+  alias / :split
+end
+
+p lt = "ky-ky! " * let
+p lt
+p ( lt / " ").size # !!!
+p "***"
+
+p ".split"
+p "ky-ky".split('-')
+p "ky-ky".split('y')
+
+p ".join"
+p ["ky","ky"].join('-')
+p ["ky","ky"].join(',')
+p ["ky","ky"].join
+p ["ky","ky"].to_s # !!!
+p "*"
+p ["ky","ky"] * '-'
+p ["ky","ky"] * 3
+p "***"
+
+p "dlinna stroki"
+p "qwertyuiop[]asdfghjkl"
+p "qwertyuiop[]asdfghjkl".size
+p "tri"
+puts "tri"
+p "tri".size
+p "***"
+
+p "poluchenie podstroki"
+p stroka = "asdfghjkl qwertyu zxcbnm 111111 222222 333333!"
+p stroka[27..-1]
+p stroka[27...-1]
+p stroka[9...14]
+p stroka[9..13]
+p stroka[3..3]
+#p stroka[3] # esli ne diapazon - > vidaetsya kod
