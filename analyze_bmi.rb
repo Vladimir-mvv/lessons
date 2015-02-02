6075c956cee877dad1193ab7519856f31f60ea1b
stealth <stealthw@mail.ru>
2015-02-02 08:00:37 +0200

    lesRB dz2

---
+++ analyze_bmi.rb
@@ -0,0 +1,22 @@
+require_relative 'bmi.rb'
+
+=begin
+Метод для определения категории человека по индексу массы тела
+  Ё°ЁЁT┌Ё-Ё+ Ё+Ё©T▐ Ё-Ё©T─ЁЁЁ+ЁЁЁ©ЁЁЁ-Ё©T▐ ЁЁЁ-T┌ЁЁЁЁЁ-T─Ё©Ё© T┤ЁЁЁ©Ё-Ё-ЁЁЁЁЁ- Ё©Ё- Ё©Ё-Ё+ЁЁЁЁT│T┐ Ё-Ё-T│T│T▀ T┌ЁЁЁ©Ё-
+  Underweight: < 18.5
+  Normal weight: 18.5 - 24.9
+  Overweight: 25 - 29.9
+  Obese: Б┴╔ 30
+
+  http://bmicalculator.cc/?gclid=COyX3qrjg78CFagSwwodtrMA1g
+=end
+
+def analyze_bmi(weight, height)
+  bmi = bmi(weight, height)
+  #TODO Ё-Ё©T─ЁЁЁ+ЁЁЁ©Ё©T┌T▄ ЁЁЁ-T┌ЁЁЁЁЁ-T─Ё©T▌ T┤ЁЁЁ©Ё-Ё-ЁЁЁЁЁ- Ё+Ё©T▐ Ё-Ё-ЁЁЁ+ЁЁЁ-Ё-Ё-ЁЁЁ- bmi
+  #TODO определить категорию человека для найденного bmi
+  
+
+
+end
+
